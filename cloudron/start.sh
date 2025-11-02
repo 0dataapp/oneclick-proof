7331d044ed4fb16c87b21eed827681c8ac09f3a6
Rosano Coutinho <github.com@xeari.com>
2025-11-02 11:47:39 +0000

    change chown order

--- cloudron/start.sh
+++ cloudron/start.sh
@@ -2,8 +2,8 @@
 
 # ensure that this file executable in the app repo: chmod +x start.sh.
 
-chown -R cloudron:cloudron /app/data
-
 mkdir -p /app/data/__local
+
+chown -R cloudron:cloudron /app/data
 
 exec /usr/local/bin/gosu cloudron:cloudron npm start
